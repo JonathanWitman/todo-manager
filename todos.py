45eee9d059dd67cb31850da2a11428e126776f6e
Jonathan Witman <jawitman8@gmail.com>
2019-02-07 14:39:24 -0500

    got some stuff working lets gooooo

--- todos.py
+++ todos.py
@@ -5,15 +5,10 @@ import datetime
 
 #Look up the datetime module
 
-class Item(object):
-    pass
-    #1$) A timestamp of when they were created.
-    #2) A boolean marking the item as completed or not.
-    #3$) And the text of the actual to-do item.
-    def add(input):
-        a = open("todos.txt","a")
-        a.write(f"{length}. " + input + " started @ "+ time + "\n")
-        a.close()
+
+#lines = ['1','2','3']
+lines = []
+time = str(datetime.datetime.now())
 
 class Manager(object):
 
@@ -21,24 +16,45 @@ class Manager(object):
         for i in range(0,len(lines)):
             print(lines[i])
 
+    def clear():
+        open('todos.txt', 'w').close()
+
+
     def update():
         a = open("todos.txt", "rt")
         for line in a:
             lines.append(line)
 
-    def done():
-        pass
-    #1) Print all of the to-do items in the list.
-    #2) Add a new item to the list.
+    def write():
+        Manager.clear()
+        a = open("todos.txt", "w")
+        for item in lines:
+            a.write(item)
+
+
+    def done(input):
+        current = lines[input - 1]
+        lines.insert(input - 1, "DONE!! " + current)
+        lines.remove(current)
+        Manager.write()
+
+    #1$) Print all of the to-do items in the list.
+    #2$) Add a new item to the list.
     #3) Mark an item as completed.
 
-lines = []
+class Item(object):
+    pass
+    #1$) A timestamp of when they were created.
+    #2) A boolean marking the item as completed or not.
+    #3$) And the text of the actual to-do item.
+    def add(input):
+        a = open("todos.txt","a")
+        a.write(f"{len(lines) + 1}. [ ] " + input + " started @ "+ time + "\n")
+        a.close()
 
-time = str(datetime.datetime.now())
-length = len(lines)
-
-Item.add('yo dude')
 Manager.update()
-#Manager.print()
-
-#print(lines[1])
+Manager.done(1)
+Manager.print()
+#Manager.write()
+#Manager.clear()
+#Item.add('yo dude')
