43edd60e50a97740ca924f4ff93d878a839e5c79
Jonathan Witman <jawitman8@gmail.com>
2019-02-07 14:53:59 -0500

    ready to convert to module formatting

--- todos.py
+++ todos.py
@@ -34,27 +34,49 @@ class Manager(object):
 
     def done(input):
         current = lines[input - 1]
-        lines.insert(input - 1, "DONE!! " + current)
-        lines.remove(current)
-        Manager.write()
+        if current.startswith("DONE!!"):
+            print('this item is already completed')
+        else:
+            lines.insert(input - 1, "DONE!! " + current)
+            lines.remove(current)
+            Manager.write()
 
     #1$) Print all of the to-do items in the list.
     #2$) Add a new item to the list.
-    #3) Mark an item as completed.
+    #3$) Mark an item as completed.
 
 class Item(object):
-    pass
+    def check(input):
+        value = lines[input - 1]
+        if value.startswith("DONE!!"):
+            print("this item is done")
+        else:
+            print("this item isn't finished quite yet")
     #1$) A timestamp of when they were created.
-    #2) A boolean marking the item as completed or not.
+    #2$) A boolean marking the item as completed or not.
     #3$) And the text of the actual to-do item.
     def add(input):
         a = open("todos.txt","a")
         a.write(f"{len(lines) + 1}. [ ] " + input + " started @ "+ time + "\n")
         a.close()
 
+#**update moves items from txt into list
 Manager.update()
-Manager.done(1)
-Manager.print()
+
+#**done marks an item as complete
+Manager.done(3)
+
+#**print prints all of the items on the list
+#Manager.print()
+
+#**write writes the list to the txt file
 #Manager.write()
+
+#**clear clears the current txt file oooooooooh
 #Manager.clear()
+
+#**add adds an item to the list
 #Item.add('yo dude')
+
+#**check checks if an item is done or not
+#Item.check(2)
